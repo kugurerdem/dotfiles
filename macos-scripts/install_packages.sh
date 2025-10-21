8cce120e81cf41261fc543c5e8d2ffbd01422c09
Ugur Erdem <k.ugurerdem@gmail.com>
2025-10-21 23:48:41 +0300

    Introduce macos-scripts folder

---
+++ macos-scripts/install_packages.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+brew install --formula neovim lazygit git-delta zellij stow fish moreutils ripgrep fzf tree rsync hugo docker lazydocker
+
+brew install --cask ghostty telegram signal
