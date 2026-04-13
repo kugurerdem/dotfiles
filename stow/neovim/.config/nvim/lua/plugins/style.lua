776ae31765c1f891cb40c9139a39fa9d6cd6829d
Ugur Erdem <k.ugurerdem@gmail.com>
2026-04-14 00:06:22 +0300

    Fix broken Neovim startup

--- stow/neovim/.config/nvim/lua/plugins/style.lua
+++ stow/neovim/.config/nvim/lua/plugins/style.lua
@@ -4,7 +4,6 @@ return {
     'vim-airline/vim-airline',
     dependencies = { 'vim-airline/vim-airline-themes' },
     config = function()
-      vim.g.airline_theme = 'catppuccin'
       vim.g.airline_powerline_fonts = 1
     end
   },
